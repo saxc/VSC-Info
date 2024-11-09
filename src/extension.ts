ba3b916bf747b5e14cb204d7b0437e84f8eaf147
saxc <saxc@users.noreply.github.com>
2024-11-09 12:56:52 +0100

    extension erstellt

--- src/extension.ts
+++ src/extension.ts
@@ -5,21 +5,12 @@ import * as vscode from 'vscode';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    const statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+    const version = vscode.version;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vsc-info" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('vsc-info.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from VSC-Info!');
-	});
-
-	context.subscriptions.push(disposable);
+    statusBar.text = "$(sparkle) " + version;
+    statusBar.tooltip = "VS Code Version: " + version;
+    statusBar.show();
 }
 
 // This method is called when your extension is deactivated
